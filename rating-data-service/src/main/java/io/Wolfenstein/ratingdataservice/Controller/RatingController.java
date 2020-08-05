cae96e5e57ac4cf25958216d97acd0b464c2e3c2
ankit <ankit@DESKTOP-A4EU1SK>
2020-08-05 13:44:23 +0530

    rating

--- rating-data-service/src/main/java/io/Wolfenstein/ratingdataservice/Controller/RatingController.java
+++ rating-data-service/src/main/java/io/Wolfenstein/ratingdataservice/Controller/RatingController.java
@@ -1,11 +1,16 @@
 package io.Wolfenstein.ratingdataservice.Controller;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.Wolfenstein.ratingdataservice.rating.Rating;
+import io.Wolfenstein.ratingdataservice.rating.UserRating;
 
 @RestController
 @RequestMapping("/rating")
@@ -15,5 +20,13 @@ public class RatingController {
 	public Rating getRating(@PathVariable String movieId) {
 		return new Rating(movieId, 4);
 		
+	}
+	
+	@GetMapping("user/{userId}")
+	public UserRating getUserRating(@PathVariable String  userId) {
+		List<Rating> rating=Arrays.asList(new Rating("123", 4),new Rating("234", 5));
+		UserRating userRating=new UserRating();
+		userRating.setRating(rating);
+		return userRating;
 	}
 }
